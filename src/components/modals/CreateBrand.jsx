bda40ded2a46eaf5d4a48bb2e44209c93688dd28
Mikey191 <desertigorb@gmail.com>
2024-07-31 11:35:41 +0300

    addType addBrand

--- src/components/modals/CreateBrand.jsx
+++ src/components/modals/CreateBrand.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
+import { createBrand } from "../../http/deviceAPI";
 
 const CreateBrand = ({ show, onHide }) => {
+  const [value, setValue] = useState("");
+  const addBrand = () => {
+    createBrand({ name: value }).then((data) => {
+      setValue("");
+      onHide();
+    });
+    onHide();
+  };
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
@@ -10,14 +20,18 @@ const CreateBrand = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder="Введите название брэнда" />
+          <Form.Control
+            placeholder="Введите название брэнда"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant={"outline-danger"} onClick={onHide}>
           Закрыть
         </Button>
-        <Button variant={"outline-success"} onClick={onHide}>
+        <Button variant={"outline-success"} onClick={addBrand}>
           Добавить
         </Button>
       </Modal.Footer>
--- src/components/modals/CreateType.jsx
+++ src/components/modals/CreateType.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
+import { createType } from "../../http/deviceAPI";
 
 const CreateType = ({ show, onHide }) => {
+  const [value, setValue] = useState("");
+  const addType = () => {
+    createType({ name: value }).then((data) => {
+      setValue("");
+      onHide();
+    });
+    onHide();
+  };
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
@@ -9,13 +19,21 @@ const CreateType = ({ show, onHide }) => {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-       <Form>
-        <Form.Control placeholder="Введите название типа"/>
-       </Form>
+        <Form>
+          <Form.Control
+            value={value}
+            placeholder="Введите название типа"
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant={"outline-danger"} onClick={onHide}>Закрыть</Button>
-        <Button variant={"outline-success"} onClick={onHide}>Добавить</Button>
+        <Button variant={"outline-danger"} onClick={onHide}>
+          Закрыть
+        </Button>
+        <Button variant={"outline-success"} onClick={addType}>
+          Добавить
+        </Button>
       </Modal.Footer>
     </Modal>
   );
